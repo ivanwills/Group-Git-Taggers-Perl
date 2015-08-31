51ecd0f5be4e457e83c8b6482622191e1b0e7d35
Ivan Wills <ivan.wills@gmail.com>
2015-08-31 19:26:25 +1000

    Updating for release of version 0.0.2

--- lib/Group/Git/Taggers/Perl.pm
+++ lib/Group/Git/Taggers/Perl.pm
@@ -16,7 +16,7 @@ use English qw/ -no_match_vars /;
 
 extends 'Group::Git::Taggers';
 
-our $VERSION = version->new('0.0.1');
+our $VERSION = version->new('0.0.2');
 
 sub match {
     my ($self, $project) = @_;
@@ -36,7 +36,7 @@ Group::Git::Taggers::Perl - Tags repositories as containing Perl code for Group:
 
 =head1 VERSION
 
-This documentation refers to Group::Git::Taggers::Perl version 0.0.1
+This documentation refers to Group::Git::Taggers::Perl version 0.0.2
 
 =head1 SYNOPSIS
 
